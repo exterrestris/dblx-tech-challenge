7e548fcb13299414d38f93ad8028d30a5cbcad79
Sean Ellingham <sean@terrestris.co.uk>
2025-07-02 17:32:03 +0100

    Dashboard fixes/adjustments
    Add Items search bar
    Fix layout issues

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -14,10 +14,12 @@ const iconMap = {
 
 <template>
     <div class="relative w-full">
-        <p class="absolute inset-x-0 bottom-0 bg-black text-white p-4">{{ item.name }}</p>
-        <div class="bg-black p-4 absolute left-0 top-0">
-            <component :is="iconMap[item.type_icon]" class="h-5 w-5 text-white"/>
+        <div class="flex absolute top-0 left-0 w-full">
+            <div class="bg-black p-4 float-left left-0 top-0">
+                <component :is="iconMap[item.type_icon]" class="h-5 w-5 text-white"/>
+            </div>
+            <p class="bg-black text-white p-4 flex-grow">{{ item.name }}</p>
         </div>
-        <img class="w-full" :src="item.image_url" :alt="item.name" />
+        <img class="object-cover" :src="item.image_url" :alt="item.name" />
     </div>
 </template>
--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -2,8 +2,8 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
-import PlaceholderPattern from '../components/PlaceholderPattern.vue';
 import Card from '@/components/Card.vue';
+import { SearchInput } from '@/components/ui/search';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -24,9 +24,11 @@ defineProps<{
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="p-6 bg-white rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                <h3 class="text-2xl">Search</h3>
+                <h3 class="text-2xl">Search Items</h3>
+
+                <SearchInput class="py-4" route-name="admin.items.index" />
             </div>
-                <div class="p-6 bg-white rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+            <div class="p-6 bg-white rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                 <h3 class="text-2xl">Latest Items</h3>
 
                 <div class="grid gap-4 grid-cols-6 mt-4">
